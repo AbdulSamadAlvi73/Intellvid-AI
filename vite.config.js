c605df2384fc7e840acfeff071a7a77cf4765746
Awaiz Shaikh <116957635+AbdulSamadAlvi73@users.noreply.github.com>
2025-02-26 01:03:57 +0500

    Frontend Completed

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    host: "192.168.7.1",
-    port: 3000,
+    host: true,
+    port: 5173,
   },
   plugins: [react(), tailwindcss()],
 });
